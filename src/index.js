4b32ca3f3ad814368ff079cbb96dd4daec07e58d
Vsevolod <vsevolod18031999@gmail.com>
2022-09-06 00:08:07 +0200

    34

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import {rerenderEntireTree} from "./render";
-import state from "./redux/state";
+import App from './App'
+import {BrowserRouter} from "react-router-dom";
+import {addPost, subscribe, updateNewPostText} from './redux/state';
+import state from './redux/state'
 
-
+let rerenderEntireTree = (state) => {
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+            </BrowserRouter>
+        </React.StrictMode>
+    );
+};
 
 rerenderEntireTree(state);
 
+subscribe(rerenderEntireTree)
 
