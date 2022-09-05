5bc201de00f182ce8d639f170745c8370822fea8
Vsevolod <vsevolod18031999@gmail.com>
2022-09-05 23:18:55 +0200

    fjcfccf

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
 import Dialogs from './components/Dialogs/Dialogs'
-import {Route, Switch} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import Settings from './components/Settings/Settings'
 import News from './components/News/News'
 
@@ -20,12 +20,12 @@ function App (props) {
             <Header/>
             <Navbar links={props.state.staticPages.links}/>
             <div className="app-wrapper-content">
-                <Switch>
-                    <Route path='/profile' render={() => <Profile posts={props.state.profilePage.posts} addPost={props.addPost} updateNewPostText={props.updateNewPostText} newPostText={props.state.profilePage.newPostText}/>}/>
-                    <Route path='/dialogs' render={() => <Dialogs dialogs={props.state.dialogsPage.dialogs} messages={props.state.dialogsPage.messages}/>}/>
-                    <Route path='/news' render={() => <News/>}/>
-                    <Route path='/settings' render={() => <Settings/>}/>
-                </Switch>
+                <Routes>
+                    <Route path='/profile' element={<Profile posts={props.state.profilePage.posts} addPost={props.addPost} updateNewPostText={props.updateNewPostText} newPostText={props.state.profilePage.newPostText}/>}/>
+                    <Route path='/dialogs' element={<Dialogs dialogs={props.state.dialogsPage.dialogs} messages={props.state.dialogsPage.messages}/>}/>
+                    <Route path='/news' element={<News/>}/>
+                    <Route path='/settings' element={ <Settings/>}/>
+                </Routes>
             </div>
         </div>
     )
