510325ba05b4509cc4743886d9df9ba06b3e5fea
Vsevolod <vsevolod18031999@gmail.com>
2022-09-09 16:59:32 +0200

    39 homework

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,8 +7,22 @@ import Messages from './Messages/Messages'
 
 const Dialogs = (props) => {
 
-    const dialogsElements = props.dialogs.map( d => (<DialogsItem name={d.name} id={d.id}/>));
-    const messagesElements = props.messages.map( m => (<Messages message={m.message}/>))
+
+
+    const dialogsElements = props.dialogs.map(d => (<DialogsItem name={d.name} id={d.id} />));
+    const messagesElements = props.messages.map(m => (<Messages message={m.message} />))
+
+    const newMessage = React.createRef();
+
+    const sendMessage = () => {
+
+    }
+
+    const messageChange = () => {
+        // const messageText = newMessage.current.value;
+
+    }
+
 
     return (
         <div className={style.dialogs}>
@@ -18,6 +32,15 @@ const Dialogs = (props) => {
             <div className={style.messages}>
                 {messagesElements}
             </div>
+            <div></div>
+            <div>
+                <textarea name="post" id="" cols="15" rows="5" className={style.textMessage} ref={newMessage} onChange={messageChange} />
+                <button className={style.sendMessageButton} onChange={sendMessage}>Send</button>
+            </div>
+            <div>
+
+            </div>
+
         </div>
     )
 
